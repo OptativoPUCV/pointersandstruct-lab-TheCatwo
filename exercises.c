4c0a9ea9cb9ffe2e04e8ae418d5ac323af6f84c7
Replit user <replituser@example.com>
2021-03-30 01:12:37 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -84,7 +84,7 @@ typedef struct {
 
 Vector * crearVector(int n) 
 {
-  Vector* v=(Vector*)malloc(sizeof(Vector)); 
+  Vector* v=(Vector*)calloc(n, sizeof(int));
    v->capacidad=n;
    return v;
 }
